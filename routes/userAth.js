9ece33c1e20da608e5c37316fc1c3e1b99b3549e
8pro <mechackkobius@gmail.com>
2023-02-13 00:06:34 +0300

    add routes fors : get all users and get a user

--- routes/userAth.js
+++ routes/userAth.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, loginUserCtrl } = require("../controllers/userCtrl");
+const {
+  createUser,
+  loginUserCtrl,
+  getAllUsers,
+  getUser,
+  deleteUser,
+} = require("../controllers/userCtrl");
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
+router.get("/all-users", getAllUsers);
+router.get("/:id", getUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
