da2534f2ecf28c4bd53e11367c300c926b588909
8pro <mechackkobius@gmail.com>
2023-02-12 14:20:42 +0300

    Making Auth to work

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const dbConnect = require("./config/dbConnectMongoose");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const authRouter = require("./routes/userAth");
+
 dbConnect();
 
-app.use("/", (req, res) => {
-  res.send(`Welcome everybody !`);
-});
+// app.use("/", (req, res) => {
+//   res.send(`Welcome everybody !`);
+// });
+
+app.use("/api/user", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening at Port: ${PORT}`);
