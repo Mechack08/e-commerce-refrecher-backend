c85fe0d2bdf3a4a82efeb5fbbff307d09331b866
8pro <mechackkobius@gmail.com>
2023-02-13 11:56:36 +0300

    Refactoring

--- routes/userAth.js
+++ routes/userAth.js
@@ -8,12 +8,13 @@ const {
   deleteUser,
   updateUser,
 } = require("../controllers/userCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getAllUsers);
-router.get("/:id", getUser);
-router.delete("/:id", deleteUser);
-router.put("/:id", updateUser);
+router.get("/:id", authMiddleware, isAdmin, getUser);
+router.delete("/:id", authMiddleware, deleteUser);
+router.put("/:id", authMiddleware, updateUser);
 
 module.exports = router;
