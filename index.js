5945d703a28003d95e42bdb3bd58e1a895e99b57
8pro <mechackkobius@gmail.com>
2023-02-13 17:06:31 +0300

    Add cookie parser

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const dbConnect = require("./config/dbConnectMongoose");
 const { notFound, errorHandler } = require("./middlewares/errorHandle");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 const authRouter = require("./routes/userAth");
 
