5ff84784e22af1c4ad705177c8b04c708e06a079
8pro <mechackkobius@gmail.com>
2023-02-17 11:50:16 +0300

    Update and delete product created. Using slugify to handle slugs while creating and updating a Product

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,8 +1,12 @@
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
 
 const createProduct = asyncHandler(async (req, res) => {
   try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
     const newProduct = await Product.create(req.body);
     res.json(newProduct);
   } catch (error) {
@@ -30,4 +34,34 @@ const getProducts = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createProduct, getProduct, getProducts };
+const updateProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req?.body?.title) req.body.slug = slugify(req.body.title);
+
+    const updateProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updateProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(req.params.id);
+    res.json(deleteProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createProduct,
+  getProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+};
