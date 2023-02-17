5083aaa14c999bce22edd148e3ba5f26e26ca272
8pro <mechackkobius@gmail.com>
2023-02-17 17:26:57 +0300

    Refactoring the function to get all products by adding limitation, sorting, which fiels to hide or show, etc.

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
+const { json } = require("body-parser");
 
 const createProduct = asyncHandler(async (req, res) => {
   try {
@@ -27,7 +28,47 @@ const getProduct = asyncHandler(async (req, res) => {
 
 const getProducts = asyncHandler(async (req, res) => {
   try {
-    const products = await Product.find();
+    const queryObj = { ...req.query };
+
+    // Filtering
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // Limitting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // Pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      console.log(productCount);
+      if (skip >= productCount) throw new Error("This page does not exist");
+    }
+    console.log(page, limit, skip);
+
+    const products = await query;
     res.json(products);
   } catch (error) {
     throw new Error(error);
