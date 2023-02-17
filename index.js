b39809b79dcd6ec486b007cf9ddb4b8641fab159
8pro <mechackkobius@gmail.com>
2023-02-17 11:48:13 +0300

    Call installed dependencies

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const express = require("express");
 const dbConnect = require("./config/dbConnectMongoose");
 const { notFound, errorHandler } = require("./middlewares/errorHandle");
 const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
+dbConnect();
+
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 const authRouter = require("./routes/userAth");
 const productRouter = require("./routes/productRoute");
-
-dbConnect();
 
 // app.use("/", (req, res) => {
 //   res.send(`Welcome everybody !`);
