126f4f2debaa66c30677448b6eb4b8a81131d11f
8pro <mechackkobius@gmail.com>
2023-02-17 11:50:56 +0300

    Add routes, update and delete

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,10 +4,15 @@ const {
   createProduct,
   getProduct,
   getProducts,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/productCtrl");
 
 route.post("/", createProduct);
 route.get("/all", getProducts);
+
 route.get("/:id", getProduct);
+route.put("/:id", updateProduct);
+route.delete("/:id", deleteProduct);
 
 module.exports = route;
